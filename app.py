58a12e51fb4d121223851d56e38bd35801edac38
lguimaraes10 <42978726+lguimaraes10@users.noreply.github.com>
2022-01-28 23:56:55 -0300

    CV_v3

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask
 
 app = Flask(__name__)
 
-csrf = CSRFProtect()
-csrf.init_app(app) # Compliant
-
 @app.route("/")
 def pagina_inicial():
     return "Hello World - Lucas Guimaraes - FIAP"
