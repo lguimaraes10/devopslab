b1e0ba223d83249bb0e6452d2688e7c90ae867cf
lguimaraes10 <42978726+lguimaraes10@users.noreply.github.com>
2022-01-29 01:27:58 -0300

    CV_2.4

--- app.py
+++ app.py
@@ -5,15 +5,17 @@ app.config['WTF_CSRF_ENABLED'] = True
 
 @app.route('/Hello World - Lucas Guimaraes - FIAP/', methods=['POST']) # Compliant
 def pagina_inicial():
+
     return "Hello World - Lucas Guimaraes - FIAP"
 
 class unprotectedForm(FlaskForm):
+    pass
     class Meta:
         csrf = True # Compliant
 
     name = TextField('name')
     submit = SubmitField('submit')
-    
+
 if __name__ == '__main__':
     app.run()
 
