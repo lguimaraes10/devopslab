d26ddf34956be87d8c75197196aee69534deaea1
lguimaraes10 <42978726+lguimaraes10@users.noreply.github.com>
2022-01-28 23:46:06 -0300

    Correção da Vulnerabilidade_V1

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask
 
 app = Flask(__name__)
 
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
+
 @app.route("/")
 def pagina_inicial():
     return "Hello World - Lucas Guimaraes - FIAP"
+
+class unprotectedForm(FlaskForm):
+    class Meta:
+        csrf = True # Compliant
+
+    name = TextField('name')
+    submit = SubmitField('submit')
 
 if __name__ == '__main__':
     app.run()
