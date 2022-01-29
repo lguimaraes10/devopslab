f070ec2103b9147cdec1a1c8632e24b89cc2ff3e
lguimaraes10 <42978726+lguimaraes10@users.noreply.github.com>
2022-01-29 02:12:22 -0300

    ok

--- app.py
+++ app.py
@@ -6,12 +6,6 @@ app = Flask(__name__)
 def pagina_inicial():
     return "Hello World - Lucas Guimaraes - FIAP"
 
-class unprotectedForm(FlaskForm):
-    class Meta:
-        csrf = True # Compliant
-
-    name = TextField('name')
-    submit = SubmitField('submit')    
 
 if __name__ == '__main__':
     app.run()
