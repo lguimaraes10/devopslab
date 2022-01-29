428ef098e185caeea7255aa267ba90f1c3d9ae33
lguimaraes10 <42978726+lguimaraes10@users.noreply.github.com>
2022-01-29 00:56:18 -0300

    original

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/example/', methods=['POST']) # Compliant
-def example():
-    return 'example'
+@app.route("/")
+def pagina_inicial():
+    return "Hello World - Lucas Guimaraes - FIAP"
 
 if __name__ == '__main__':
     app.run()
