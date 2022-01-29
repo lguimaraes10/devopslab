4944b04b737f894eb5b916889f8431d494003186
lguimaraes10 <42978726+lguimaraes10@users.noreply.github.com>
2022-01-29 00:53:43 -0300

    CV_14

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
-csrf = CSRFProtect(app)
-csrf.init_app(app) # Compliant
 
 @app.route('/example/', methods=['POST']) # Compliant
 def example():
     return 'example'
-
-class unprotectedForm(FlaskForm):
-    class Meta:
-        csrf = True # Compliant
-
-    name = TextField('name')
-    submit = SubmitField('submit')
 
 if __name__ == '__main__':
     app.run()
