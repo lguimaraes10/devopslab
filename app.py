76f5c7116ad53a77fef7ca9e99d21c87bf9ec058
lguimaraes10 <42978726+lguimaraes10@users.noreply.github.com>
2022-01-29 01:46:59 -0300

    cv0003

--- app.py
+++ app.py
@@ -1,19 +1,13 @@
 from flask import Flask
 from flask_wtf.csrf import CsrfProtect
 app = Flask(__name__)
-app.secret_key = 'very secret'
-CsrfProtect(app)
+class Config:
+    WTF_CSRF_ENABLED = True
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
 
 @app.route('/Hello World - Lucas Guimaraes - FIAP/', methods=['POST']) # Compliant
 def pagina_inicial():
     return "Hello World - Lucas Guimaraes - FIAP"
-
-class unprotectedForm(FlaskForm):
-    class Meta:
-        csrf = True # Compliant
-
-    name = TextField('name')
-    submit = SubmitField('submit')
 
 if __name__ == '__main__':
     app.run()
