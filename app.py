f53a245851a84b7a8edaa49835a4362ecad9ee97
lguimaraes10 <42978726+lguimaraes10@users.noreply.github.com>
2022-01-29 00:51:25 -0300

    CV_14

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
 app = Flask(__name__)
-csrf = CSRFProtect()
+csrf = CSRFProtect(app)
 csrf.init_app(app) # Compliant
 
 @app.route('/example/', methods=['POST']) # Compliant
