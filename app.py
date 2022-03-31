50532cbfabda9692443875277dd8322235776d21
lguimaraes10 <42978726+lguimaraes10@users.noreply.github.com>
2022-03-30 23:52:03 -0300

    Correcao_http

--- app.py
+++ app.py
@@ -1,18 +1,13 @@
 from flask import Flask
 
 app = Flask(__name__)
-app.config['WTF_CSRF_ENABLED'] = False
+
+csrf = CSRFProtect()
+csrf.init_app(app) # Compliant
 
 @app.route("/")
 def pagina_inicial():
     return "Hello World - 30/03/2022 - FIAP"
-
-class unprotectedForm(FlaskForm):
-    class Meta:
-        csrf = True # Compliant
-
-    name = TextField('name')
-    submit = SubmitField('submit')
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
